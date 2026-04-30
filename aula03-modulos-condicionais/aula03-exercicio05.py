372c64a1d11b17c11236d4ad7587fb59ca8c16aa
doern-felipe <doern.felipe21@gmail.com>
2026-04-30 07:53:11 -0300

    Feito exercício 05 da aula 03

--- aula03-modulos-condicionais/aula03-exercicio05.py
+++ aula03-modulos-condicionais/aula03-exercicio05.py
@@ -8,3 +8,10 @@
 # ▪ Então se observa que os múltiplos de um número são divisíveis por esse número, então o resto dessa
 # divisão será 0.
 
+A = int(input("Digite o valor de A: "))
+B = int(input("Digite o valor de B: "))
+
+if A % B == 0 or B % A == 0:
+    print("São Múltiplos")
+else:
+    print("Não são Múltiplos")
